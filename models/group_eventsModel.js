b37d3482740ff38e68e1d67432210b945278555e
Nana Hauwa Abdulsalam <68066734+Amal-Salam@users.noreply.github.com>
2023-09-19 22:13:06 +0100

    Update group_eventsModel.js

--- models/group_eventsModel.js
+++ models/group_eventsModel.js
@@ -1,16 +1,22 @@
+const Event = require('./eventModel'); 
+const Group = require('./groupModel');
 module.exports = (sequelize, Datatypes) => {
   // defines the columns for the group_events table
   const Group_events = sequelize.define('group_events', {
    
   event_id: {
-      type: Datatypes.NUMERIC,
+      type: Datatypes.INTEGER,
       allowNull: false,
     },
     group_id: {
-      type: Datatypes.NUMERIC,
+      type: Datatypes.INTEGER,
       allowNull: false,
     },
   });
+
+  // Define the foreign key relationships
+  Group_events.belongsTo(Event, { foreignKey: 'event_id' });
+  Group_events.belongsTo(Group, { foreignKey: 'group_id' });
 
   return Group_events;
 };
