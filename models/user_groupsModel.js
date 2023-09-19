f07d6cb7dc5bc9c6d9ef57ab4109c60745299c56
Nana Hauwa Abdulsalam <68066734+Amal-Salam@users.noreply.github.com>
2023-09-19 22:07:03 +0100

    Update user_groupsModel.js

--- models/user_groupsModel.js
+++ models/user_groupsModel.js
@@ -1,16 +1,24 @@
+const User = require('./userModel'); 
+const Group = require('./groupModel');
+
 module.exports = (sequelize, Datatypes) => {
   // defines the columns for the user_groups table
   const User_groups = sequelize.define('user_groups', {
    
   user_id: {
-      type: Datatypes.NUMERIC,
+      type: Datatypes.INTEGER,
       allowNull: false,
     },
     group_id: {
-      type: Datatypes.NUMERIC,
+      type: Datatypes.INTEGER,
       allowNull: false,
     },
   });
+  
+// Define the foreign key relationships
+  User_groups.belongsTo(User, { foreignKey: 'user_id' });
+  User_groups.belongsTo(Group, { foreignKey: 'group_id' });
+
 
   return User_groups;
 };
