8868f40016dedcf37717e9602abfcf053319041e
Nana Hauwa Abdulsalam <68066734+Amal-Salam@users.noreply.github.com>
2023-09-19 21:17:55 +0100

    Update and rename userGroupsModel.js to user_groupsModel.js

--- models/user_groupsModel.js
+++ models/user_groupsModel.js
@@ -1,16 +1,16 @@
 module.exports = (sequelize, Datatypes) => {
   // defines the columns for the user_groups table
-  const UserGroups = sequelize.define('userGroups', {
+  const User_groups = sequelize.define('user_groups', {
    
   user_id: {
-      type: Datatypes.STRING,
+      type: Datatypes.NUMERIC,
       allowNull: false,
     },
     group_id: {
-      type: Datatypes.STRING,
+      type: Datatypes.NUMERIC,
       allowNull: false,
     },
   });
 
-  return UserGroups;
+  return User_groups;
 };
