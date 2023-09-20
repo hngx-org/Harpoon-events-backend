a7fb342420038d592f3e921e168b684a10aa3815
Bakare Abiola <iamstarcode@gmail.com>
2023-09-20 12:29:45 +0100

    feat: add validation over signup route

--- validations/signupValidationSchema.js
+++ validations/signupValidationSchema.js
@@ -1,9 +1,9 @@
 const Joi = require('joi');
 
-const registerValidationSchema = Joi.object().keys({
+const signupValidationSchema = Joi.object().keys({
   name: Joi.string().required().min(5),
   email: Joi.string().required().email(),
-  image: Joi.string().required(),
+  image: Joi.string().optional(),
   password: Joi.string().required().min(3),
 });
 
