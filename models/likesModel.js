f855ae3f2bbe5212196e40c2f43f499afca9a16c
chriss1525 <okothchris15@gmail.com>
2023-09-20 21:34:38 +0300

    define likes model and its associations

---
+++ models/likesModel.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const Like = sequelize.define('likes', {
+    // No need to define an 'id' column, as Sequelize will create it automatically for many-to-many associations
+    user_id: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+    comment_id: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      references: {
+        model: 'comments',
+        key: 'id',
+      },
+    },
+  });
+
+  Like.associate = (models) => {
+    // Define associations as needed
+    Like.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      as: 'user',
+    });
+
+    Like.belongsTo(models.Comment, {
+      foreignKey: 'comment_id',
+      as: 'comment',
+    });
+  };
+
+  return Like;
+};
+
