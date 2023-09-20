7315e68e0a145ac3ddd95d2e94de4827bd0c5e57
chriss1525 <okothchris15@gmail.com>
2023-09-20 21:07:58 +0300

    handle connection model comment_images

---
+++ models/comment_imagesModel.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+  const CommentImages = sequelize.define('comment_images', {
+    // No need to define an 'id' column, as Sequelize will create it automatically for many-to-many associations
+    comment_id: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      references: {
+        model: 'comments',
+        key: 'id',
+      },
+    },
+    image_id: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      references: {
+        model: 'images',
+        key: 'id',
+      },
+    },
+  });
+
+  // Define associations
+  CommentImages.associate = (models) => {
+    CommentImages.belongsTo(models.Comment, {
+      foreignKey: 'comment_id',
+    });
+    CommentImages.belongsTo(models.Image, {
+      foreignKey: 'image_id',
+    });
+  };
+
+  return CommentImages;
+};
+
