77c60ebd3a0bade7ab47cea57d3ebf9f1d615252
ayobamy <ayobamyahmed@gmail.com>
2023-09-20 21:57:05 +0100

    [Update]: add image to comment endpoint

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const multer = require('multer');
 
 const eventController = require('../controllers/eventController');
 const authController = require('../controllers/authController');
 const commentController = require('../controllers/commentController');
 
 const router = express.Router();
+
+const upload = multer({ dest: 'uploads/' });
 
 // middleware to check if user is logged in
 router.use(authController.protect);
@@ -25,11 +28,15 @@ router
 router
   .route('/events/:eventId/comments')
   .post(commentController.createComment)
-.get(commentController.getAllComments);
+  .get(commentController.getAllComments);
 // Update a Comment:
 //Delete a Comment:
 
-  
+// comments with images
+router
+  .route('/comments/:commentId/images')
+  .post(upload.single('image'), commentController.addImageToComments)
+  .get(commentController.getImagesfromComments);
 
 
 module.exports = router;
