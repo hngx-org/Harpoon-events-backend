9ef14e7d796df83870ff77a165f56e0a11acd652
Tobi kin <44758414+Dxtobi@users.noreply.github.com>
2023-09-20 07:30:44 -0700

    Update comment.service.js

--- services/comment.service.js
+++ services/comment.service.js
@@ -1,7 +1,7 @@
 /**
  * Retrieves all comments for a specific event.
  *
- * @param {number} event_id - The ID of the event for which to retrieve comments.
+ * @param {any} event_id - The ID of the event for which to retrieve comments.
  * @returns {Promise<Array<Object>>} A promise that resolves to an array of comments associated with the event.
  * @throws {Error} If there's an error while fetching the comments.
  */
