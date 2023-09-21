47a687075fc19b504812da1e9f8d134f48235492
AirZED <regnalukpabio@gmail.com>
2023-09-21 08:38:54 +0100

    basic change

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,8 +15,10 @@ router.post('/twitter', authController.Twitter);
 // middleware to check if user is logged in
 router.use(requireAuth);
 
-router.get('/', userController.getUser);
-router.put('/:id', userController.updateUser);
+router
+  .route('/:userId')
+  .put(userController.updateUser)
+  .get(userController.getUser);
 
 // user expresses interest in an event
 router.post('/:userId/interests/:eventId', userController.interestedEvent);
