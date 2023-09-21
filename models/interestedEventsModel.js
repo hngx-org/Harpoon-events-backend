997d0b5db600cdb7e28caf6d27be07108b7e6f9c
chriss1525 <okothchris15@gmail.com>
2023-09-21 15:14:45 +0300

    add unique constraints to interested events table so user cannot show interest more than once

--- models/interestedEventsModel.js
+++ models/interestedEventsModel.js
@@ -2,23 +2,32 @@ module.exports = (sequelize, DataTypes) => {
   const InterestedEvent = sequelize.define('interested_events', {
     // No need to define an 'id' column, as Sequelize will create it automatically for many-to-many associations
     event_id: {
-      type: DataTypes.STRING,
-      defaultValue: DataTypes.UUIDV4,
+      type: DataTypes.UUID,
       allowNull: false,
       references: {
         model: 'events',
         key: 'id',
       },
+      unique: 'unique_event_user_interest',
     },
     user_id: {
-      type: DataTypes.STRING,
-      defaultValue: DataTypes.UUIDV4,
+      type: DataTypes.UUID,
       allowNull: false,
       references: {
         model: 'users',
         key: 'id',
       },
+      unique: 'unique_event_user_interest',
     },
+  }, {
+    // Define a unique constraint name for the combination of event_id and user_id
+    indexes: [
+      {
+        unique: true,
+        fields: ['event_id', 'user_id'],
+        name: 'unique_event_user_interest',
+      },
+    ],
   });
 
   InterestedEvent.associate = (models) => {
@@ -36,3 +45,4 @@ module.exports = (sequelize, DataTypes) => {
 
   return InterestedEvent;
 };
+
