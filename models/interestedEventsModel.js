6a666bda0112d221f7228f95ca6eeae71bb8d3c5
chriss1525 <okothchris15@gmail.com>
2023-09-21 17:07:57 +0300

    remove unique key index

--- models/interestedEventsModel.js
+++ models/interestedEventsModel.js
@@ -2,32 +2,23 @@ module.exports = (sequelize, DataTypes) => {
   const InterestedEvent = sequelize.define('interested_events', {
     // No need to define an 'id' column, as Sequelize will create it automatically for many-to-many associations
     event_id: {
-      type: DataTypes.UUID,
+      type: DataTypes.STRING,
+      defaultValue: DataTypes.UUIDV4,
       allowNull: false,
       references: {
         model: 'events',
         key: 'id',
       },
-      unique: 'unique_event_user_interest',
     },
     user_id: {
-      type: DataTypes.UUID,
+      type: DataTypes.STRING,
+      defaultValue: DataTypes.UUIDV4,
       allowNull: false,
       references: {
         model: 'users',
         key: 'id',
       },
-      unique: 'unique_event_user_interest',
     },
-  }, {
-    // Define a unique constraint name for the combination of event_id and user_id
-    indexes: [
-      {
-        unique: true,
-        fields: ['event_id', 'user_id'],
-        name: 'unique_event_user_interest',
-      },
-    ],
   });
 
   InterestedEvent.associate = (models) => {
@@ -45,4 +36,3 @@ module.exports = (sequelize, DataTypes) => {
 
   return InterestedEvent;
 };
-
