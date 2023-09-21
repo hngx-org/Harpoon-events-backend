96d6d594d03093951ff62f6e390503af3e51495c
AirZED <regnalukpabio@gmail.com>
2023-09-21 22:57:25 +0100

    minor bug

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,7 +14,6 @@ const {
   Google,
   Twitter,
 } = require('../controllers/authController');
-const requireAuth = require('../middleware/requireAuth');
 
 router.post('/signup', signup);
 router.post('/login', login);
